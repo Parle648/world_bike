6ff64b8b3e1868839021c00e941989a3f68b0396
Parle648 <sashahoumi@gmail.com>
2024-01-11 17:29:48 +0200

    Create WorkshopHeroSection UI + responsive

--- src/pages/BicycleWorkshop/BicycleWorkshop.tsx
+++ src/pages/BicycleWorkshop/BicycleWorkshop.tsx
@@ -1,9 +1,11 @@
 import TopHeader from "../../widgets/TopHeader/TopHeader";
+import WorkshopHeroSection from "../../widgets/WorkshopHeroSection/WorkshopHeroSection";
 
 const BicycleWorkshop = () => {
     return (
         <div>
             <TopHeader />
+            <WorkshopHeroSection />
         </div>
     );
 };
