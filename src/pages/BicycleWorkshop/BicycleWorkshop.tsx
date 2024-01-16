06c97a0ed152835ab080c77b54fc51a28429f66d
Parle648 <sashahoumi@gmail.com>
2024-01-16 18:21:53 +0200

    Create Footer
    Changes to be committed:
    * modified: src/Scss/global/global.scss
    * new file: src/imgs/mail.svg
    * new file: src/imgs/navigation.svg
    * new file: src/imgs/phone.svg
    * modified: src/pages/BicycleWorkshop/BicycleWorkshop.tsx
    * new file: src/widgets/Footer/Footer.tsx
    * new file: src/widgets/Footer/styles/footer.module.scss

--- src/pages/BicycleWorkshop/BicycleWorkshop.tsx
+++ src/pages/BicycleWorkshop/BicycleWorkshop.tsx
@@ -1,6 +1,7 @@
 import AdditionalService from "../../widgets/AdditionalService/AdditionalService";
 import BasicServiceCost from "../../widgets/BasicServiceCost/BasicServiceCost";
 import CompaniesSlider from "../../widgets/CompaniesSlider/CompaniesSlider";
+import Footer from "../../widgets/Footer/Footer";
 import Newsletter from "../../widgets/Newsletter/Newsletter";
 import TopHeader from "../../widgets/TopHeader/TopHeader";
 import WorkshopHeroSection from "../../widgets/WorkshopHeroSection/WorkshopHeroSection";
@@ -14,6 +15,7 @@ const BicycleWorkshop = () => {
             <AdditionalService />
             <CompaniesSlider />
             <Newsletter />
+            <Footer />
         </div>
     );
 };
