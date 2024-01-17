29b1a06f58adb29e27156c9e25182966963e916b
Parle648 <sashahoumi@gmail.com>
2024-01-17 15:44:46 +0200

    Create basic structure for GuaranteePage
    Changes:
    * Import TopHeader into GuaranteePage
    * Create GuaranteeHeroSection into GuaranteePage
    * Import CompaniesSlider into GuaranteePage
    * Import Newsletter into GuaranteePage
    * Import Footer into GuaranteePage
    
    Changes to be committed:
    * new file: src/imgs/guarantee-hero-section.png
    * modified: src/pages/Guarantee/Guarantee.tsx
    * new file: src/widgets/GuaranteeHeroSection/GuaranteeHeroSection.tsx
    * new file: src/widgets/GuaranteeHeroSection/styles/guaranteeHeroSection.module.scss

--- src/pages/Guarantee/Guarantee.tsx
+++ src/pages/Guarantee/Guarantee.tsx
@@ -1,7 +1,17 @@
+import CompaniesSlider from "../../widgets/CompaniesSlider/CompaniesSlider";
+import Footer from "../../widgets/Footer/Footer";
+import GuaranteeHeroSection from "../../widgets/GuaranteeHeroSection/GuaranteeHeroSection";
+import Newsletter from "../../widgets/Newsletter/Newsletter";
+import TopHeader from "../../widgets/TopHeader/TopHeader";
+
 const Guarantee = () => {
     return (
         <div>
-            Guarantee
+            <TopHeader />
+            <GuaranteeHeroSection />
+            <CompaniesSlider />
+            <Newsletter />
+            <Footer />
         </div>
     );
 };
