36ac30f575eb8cf2d30b12558e13675899a741bc
Parle648 <sashahoumi@gmail.com>
2024-01-18 16:32:37 +0200

    Create basic structure for ContactsPage

--- src/pages/Contacts/Contacts.tsx
+++ src/pages/Contacts/Contacts.tsx
@@ -1,7 +1,17 @@
+import CompaniesSlider from "../../widgets/CompaniesSlider/CompaniesSlider";
+import ContactsHeroSection from "../../widgets/ContactsHeroSection/ContactsHeroSection";
+import Footer from "../../widgets/Footer/Footer";
+import Newsletter from "../../widgets/Newsletter/Newsletter";
+import TopHeader from "../../widgets/TopHeader/TopHeader";
+
 const Contacts = () => {
     return (
         <div>
-            Contacts
+            <TopHeader />
+            <ContactsHeroSection />
+            <CompaniesSlider />
+            <Newsletter />
+            <Footer />
         </div>
     );
 };
