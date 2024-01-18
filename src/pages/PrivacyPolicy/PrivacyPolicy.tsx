5317cb00a3fdb1c0706b4ae9d82e7381c6e85ec8
Parle648 <sashahoumi@gmail.com>
2024-01-18 14:27:42 +0200

    Create PrivacyPolicePage
    Changes to be committed:
    * modified: src/pages/PrivacyPolicy/PrivacyPolicy.tsx
    * new file: src/widgets/PrivacyPolicy/PrivacyPolice.tsx
    * new file: src/widgets/PrivacyPolicy/styles/privacyPolice.module.scss

--- src/pages/PrivacyPolicy/PrivacyPolicy.tsx
+++ src/pages/PrivacyPolicy/PrivacyPolicy.tsx
@@ -1,7 +1,17 @@
+import CompaniesSlider from "../../widgets/CompaniesSlider/CompaniesSlider";
+import Footer from "../../widgets/Footer/Footer";
+import Newsletter from "../../widgets/Newsletter/Newsletter";
+import PrivacyPolice from "../../widgets/PrivacyPolicy/PrivacyPolice";
+import TopHeader from "../../widgets/TopHeader/TopHeader";
+
 const PrivacyPolicy = () => {
     return (
         <div>
-            PrivacyPolicy
+            <TopHeader />
+            <PrivacyPolice />
+            <CompaniesSlider />
+            <Newsletter />
+            <Footer />
         </div>
     );
 };
