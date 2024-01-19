4c2db9a3b9aa894bcf5a0b1020b9960fda7acbdd
Parle648 <sashahoumi@gmail.com>
2024-01-19 15:33:03 +0200

    Create DeliveryAndPayment basic layout

--- src/pages/DeliveryAndPayment/DeliveryAndPayment.tsx
+++ src/pages/DeliveryAndPayment/DeliveryAndPayment.tsx
@@ -1,7 +1,15 @@
+import CompaniesSlider from "../../widgets/CompaniesSlider/CompaniesSlider";
+import Footer from "../../widgets/Footer/Footer";
+import Newsletter from "../../widgets/Newsletter/Newsletter";
+import TopHeader from "../../widgets/TopHeader/TopHeader";
+
 const DeliveryAndPayment = () => {
     return (
         <div>
-            DeliveryAndPayment
+            <TopHeader />
+            <CompaniesSlider />
+            <Newsletter />
+            <Footer />
         </div>
     );
 };
