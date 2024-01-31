101a714c68cef7222a664983001e158974c8acb7
Olexandr Morozov <106087433+Parle648@users.noreply.github.com>
2024-01-31 16:25:52 +0200

    Add files via upload

---
+++ src/entities/ProductCard/ProductCard.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import styles from './styles/productCard.module.scss';
+
+type ProductCardType = {
+    country: string,
+    soldOut: boolean,
+    image: string,
+    title: string,
+    cost: string,
+}
+
+const ProductCard = ({country, soldOut, image, title, cost}: ProductCardType) => {
+    return (
+        <div className={styles.block}>
+            <img className={styles.flag} src={country} alt="country" />
+            <span className={soldOut ? styles.soldOut : styles.hasAtShop}>{soldOut ? 'Распродано' : 'В наличии'}</span>
+            <img className={styles.img} src={image} alt="image" />
+            <h3 className={styles.ttl}>{title}</h3>
+            <h4 className={styles.cost}>{cost}</h4>
+            <button className={styles.seeMore}>В 1 клик</button>
+        </div>
+    );
+};
+
+export default ProductCard
