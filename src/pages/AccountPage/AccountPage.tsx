3ec266400414d756245d44a1d1c9db232668939f
Parle648 <morozovalex842@gmail.com>
2024-02-13 15:15:38 +0200

    Create basic template for AccountPage

---
+++ src/pages/AccountPage/AccountPage.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Footer from '../../widgets/Footer/Footer';
+import TopHeader from '../../widgets/TopHeader/TopHeader';
+import Newsletter from '../../widgets/Newsletter/Newsletter';
+
+const AccountPage = () => {
+    return (
+        <>
+            <TopHeader />
+            <Newsletter />
+            <Footer /> 
+        </>
+    );
+};
+
+export default AccountPage;
