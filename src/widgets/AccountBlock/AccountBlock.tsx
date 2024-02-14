9b7fe471c712187a31148bd768faac1b8f17cd30
Parle648 <morozovalex842@gmail.com>
2024-02-14 18:10:55 +0200

    Create chooseSection logic at AccountBlock

--- src/widgets/AccountBlock/AccountBlock.tsx
+++ src/widgets/AccountBlock/AccountBlock.tsx
@@ -1,42 +1,43 @@
-import React from 'react';
+import React, { ReactHTMLElement, ReactNode } from 'react';
 import styles from './styles/accountBlock.module.scss';
 import PersonalDataForm from '../../features/PersonalDataForm/PersonalDataForm';
-import ProductSnippet from '../../entities/ProductSnippet/ProductSnippet';
-
-import bike from '../../imgs/bike-img.png';
-import DropDown from '../../features/OrderDropDown/OrderDropDown';
 import OrdersHistory from '../OrdersHistory/OrdersHistory';
 import ChangePasswodForm from '../../features/ChangePasswordForm/ChangePasswodForm';
 
-const BodyTrigger = () => {
-    return (
-        <>
-            <span ></span>
-        </>
-    );
+const bodyTrigger: ObjectInterface = {
+    history: <OrdersHistory />,
+    personal: <PersonalDataForm />,
+    password: <ChangePasswodForm />,
+}
+
+interface ObjectInterface {
+    [key: string]: ReactNode;
 }
 
 const AccountBlock = () => {
+    const [section, setSection] = React.useState("history")
+
+    function chooseSection(event: React.MouseEvent<HTMLElement>) {
+        const dataName = event.currentTarget.dataset.name || 'nothing';
+        setSection(dataName);
+    }
+
     return (
         <div className='wrapper'>
             <main className={styles.block}>
                 <div className={styles.navigationBlock}>
                     <h2 className={styles.ttl}>Мой аккаунт</h2>
                     <ul className={styles.navigation}>
-                        <li className={styles.navSection}>История заказов</li>
-                        <li className={styles.navSection}>Персональные данные</li>
-                        <li className={`${styles.navSection} ${styles.navSectionActive}`}>Смена пароля</li>
-                        <li className={styles.navSection}>Список желаний</li>
+                        <li className={`${styles.navSection} ${section === 'history' && styles.navSectionActive}`} data-name='history' onClick={chooseSection}>История заказов</li>
+                        <li className={`${styles.navSection} ${section === 'personal' && styles.navSectionActive}`} data-name='personal' onClick={chooseSection}>Персональные данные</li>
+                        <li className={`${styles.navSection} ${section === 'password' && styles.navSectionActive}`} data-name='password' onClick={chooseSection}>Смена пароля</li>
+                        <li className={`${styles.navSection} ${section === 'prefer_list' && styles.navSectionActive}`} data-name='prefer_list'>Список желаний</li>
                         <li >
                             <button className={styles.quitBtn}>Выйти</button>
                         </li>
                     </ul>
                 </div>
-                {/* <PersonalDataForm /> */} 
-                {/* <OrdersHistory /> */}
-
-                <ChangePasswodForm />
-                
+                {bodyTrigger[section] && bodyTrigger[section]}
             </main>
         </div>
     );
