0c2f4772f44934254d0fd30481b217d83eaabb37
Parle648 <morozovalex842@gmail.com>
2024-02-16 09:04:15 +0200

    Fix styles for HeaderNavigation drop-downs

--- src/widgets/MainPage/MainHerosection/MainHeroSection.tsx
+++ src/widgets/MainPage/MainHerosection/MainHeroSection.tsx
@@ -4,23 +4,11 @@ import background from '../../../imgs/main-background.png';
 import Header from '../Header/Header';
 import { Link } from 'react-router-dom';
 
+
+
 const MainHeroSection = () => {
-    const [sliderBlocks, setSliderBlocks] = React.useState([true, false, false])
-
-    // setInterval(() => {
-    //     setSliderBlocks(prev => {
-    //         if (prev[0]) {
-    //             return [false, true, false]
-    //         } else if (prev[1]) {
-    //             return [false, false, true]
-    //         } else {
-    //             return [true, false, false]
-    //         };
-    //     });
-    // }, 2000)
-
     return (
-        <div className={styles.block}>
+        <div className={styles.block} style={{background: `url(${background})`, backgroundRepeat: 'no-repeat'}}>
             <Header />
             <div className="wrapper">
                 <h1 className={styles.mainTtl}>Электро велосипеды</h1>
@@ -30,27 +18,26 @@ const MainHeroSection = () => {
                 </p>
                 <Link className={styles.btn} to='#!'>Подробнее</Link>
             </div>
-            <img className={styles.background} src={background} alt="background" />
             <div className={styles.sliderContainer}>
                 <div className={styles.slider}>
                     <div className={styles.sliderPoints}>
-                        <div className={`${styles.point} ${sliderBlocks[0] && styles.pointActive}`}></div>
-                        <div className={`${styles.point} ${sliderBlocks[1] && styles.pointActive}`}></div>
-                        <div className={`${styles.point} ${sliderBlocks[2] && styles.pointActive}`}></div>
+                        <div className={`${styles.point} ${styles.pointActive}`}></div>
+                        <div className={`${styles.point} ${styles.pointActive}`}></div>
+                        <div className={`${styles.point} ${styles.pointActive}`}></div>
                     </div>
-                    <div className={`${styles.sliderElem} ${sliderBlocks[0] && styles.sliderElemActive}`}>
+                    <div className={`${styles.sliderElem} ${styles.sliderElemActive}`}>
                         <p className={styles.sliderElemDescription}>
                             Экстремальное вождение на горном велосипеде
                         </p>
                         <Link className={styles.link} to='#!'>Подробнее</Link>
                     </div>
-                    <div className={`${styles.sliderElem} ${styles.gray} ${sliderBlocks[1] && styles.sliderElemActive}`}>
+                    <div className={`${styles.sliderElem} ${styles.gray} ${styles.sliderElemActive}`}>
                         <p className={styles.sliderElemDescription}>
                             Велосипеды для профессионалов
                         </p>
                         <Link className={styles.link} to='#!'>Подробнее</Link>
                     </div>
-                    <div className={`${styles.sliderElem} ${styles.lightGray} ${sliderBlocks[2] && styles.sliderElemActive}`}>
+                    <div className={`${styles.sliderElem} ${styles.lightGray} ${styles.sliderElemActive}`}>
                         <p className={styles.sliderElemDescription}>
                             Долгая поездка на шоссейном велосипеде
                         </p>
