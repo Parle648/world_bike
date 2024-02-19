b168e49d987c31e1bbecae09459df982f8184721
Parle648 <morozovalex842@gmail.com>
2024-02-19 16:28:00 +0200

    Add interactive for SortByFeature

--- src/features/SortByFeature/SortByFeature.tsx
+++ src/features/SortByFeature/SortByFeature.tsx
@@ -3,11 +3,27 @@ import styles from './styles/sortByfeature.module.scss';
 import arrow from '../../imgs/filters-arrow.svg';
 
 const SortByFeature = () => {
+
+    const [opened, setOpened] =React.useState(false);
+
+    function listToggle() {
+        setOpened(!opened);
+    };
+
     return (
         <div>
-            <button className={styles.openFilters}><span className={styles.title}>Сортировка от последнего</span> <img className={styles.arrow} src={arrow} alt="arrow" /></button>
+            <button className={styles.openFilters} onClick={listToggle} >
+                <span className={styles.title}>Сортировка от последнего</span> 
+                <img className={`${styles.arrow} ${opened && styles.arrowActive}`} src={arrow} alt="arrow" />
+            </button>
+            <ul className={`${styles.list} ${opened && styles.listOpened}`}>
+                <li className={styles.listItem}>По популярности</li>
+                <li className={styles.listItem}>По рейтингу</li>
+                <li className={styles.listItem}>Цены: по возрастанию</li>
+                <li className={styles.listItem}>Цены: по убыванию</li>
+            </ul>
         </div>
-    );
+    ); 
 };
 
 export default SortByFeature;
