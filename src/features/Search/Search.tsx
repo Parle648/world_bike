66f4fb74948281a22b7e39ccdd7c2546e4b7a661
Parle648 <morozovalex842@gmail.com>
2024-02-19 15:29:08 +0200

    Add interactive for Search feature

--- src/features/Search/Search.tsx
+++ src/features/Search/Search.tsx
@@ -3,9 +3,19 @@ import styles from './styles/search.module.scss';
 import search from '../../imgs/search.svg';
 
 const Search = () => {
+    const [opened, setOpened] = React.useState(false);
+
+    function toggleForm() {
+        setOpened(!opened);
+    };
+
     return (
         <div className={styles.block}>
-            <button className={styles.searchTrigger}><img src={search} alt="search" /></button>
+            <button className={styles.searchTrigger} onClick={toggleForm}><img src={search} alt="search" /></button>
+            <form className={`${styles.form} ${opened && styles.formActive}`}>
+                <input className={styles.input} type="text" onBlur={toggleForm} />
+                <img className={styles.inputSearch} src={search} alt="search" />
+            </form>
         </div>
     );
 };
