0255ebb62cc36d4d0d877794c14f4ff25a6fc2a3
Parle648 <sashahoumi@gmail.com>
2024-03-14 17:13:25 +0200

    AdCreate validation for QuickOrderForm

--- src/features/QuickOrderForm/QuickOrderForm.tsx
+++ src/features/QuickOrderForm/QuickOrderForm.tsx
@@ -2,16 +2,59 @@ import React from 'react';
 import styles from './styles/quickOrderForm.module.scss';
 import { Link } from 'react-router-dom';
 import quickOrderFormProps from './types/quickOrderForm';
+import { useForm } from 'react-hook-form';
+
+type QuickOrderForm = {
+    name: string,
+    phone: string,
+}
 
 const QuickOrderForm = ({id}: quickOrderFormProps) => {
+    
+    const {
+        register,
+        handleSubmit,
+        formState: {
+            errors
+        },
+    } = useForm<QuickOrderForm>();
+
+    function postQuickOrder(data: QuickOrderForm) {
+        console.log(data);
+    };
+
     return (
-        <form className={styles.form}>
+        <form className={styles.form} onSubmit={handleSubmit(postQuickOrder)}>
             <p className={styles.description}>
                 Укажите ваше имя и телефон, и наш менеджер 
                 свяжется с вами для оформления заказа.
             </p>
-            <input className={styles.input} type="text" placeholder='Имя' />
-            <input className={styles.input} type="text" placeholder='Телефон' />
+            <label className={styles.label}>
+                <input className={`${styles.input} ${errors.name && styles.invalidInput}`} 
+                type="text" 
+                placeholder='Имя'
+                {...register('name', {
+                    required: 'this field shouldnt be empty',
+                    pattern: {
+                        value: /^[А-ЯЁ][а-яё]*$/,
+                        message: 'Iмя повинно бути мати наст вигляд Олександр'
+                    }
+                })} />
+                {errors.name && <h2 className={styles.error}>{errors.name.message}</h2>}
+            </label>
+            <label className={styles.label}>
+                <input className={`${styles.input} ${errors.phone && styles.invalidInput}`} 
+                type="text" 
+                placeholder='Телефон'
+                {...register('phone', {
+                    required: 'this field shouldnt be empty',
+                    pattern: {
+                        value: /^\+380\d{9}$/,
+                        message: 'Введiть номер у виглядi +380554832010',
+                    }
+                })} />
+                {errors.phone && <h2 className={styles.error}>{errors.phone.message}</h2>}
+            </label>
             <p className={styles.privacyPolicy}>
                 Нажимая на кнопку «Заказать» я даю своё согласие на обработку персональных данных и принимаю
                 <Link className={styles.privacyPolicyLink} to='privacy-policy'> условия соглашения</Link>
