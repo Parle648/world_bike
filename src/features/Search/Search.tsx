f9c272a7f7ca43b176c5149fbd5f02bc9b2eba75
Parle648 <sashahoumi@gmail.com>
2024-03-19 20:50:40 +0200

    Create change store + redirect logic for SearchFeature

--- src/features/Search/Search.tsx
+++ src/features/Search/Search.tsx
@@ -1,6 +1,10 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import styles from './styles/search.module.scss';
 import search from '../../imgs/search.svg';
+import { useForm } from 'react-hook-form';
+import { setParam } from '../../shared/lib/slices/currentFilers';
+import { useDispatch } from 'react-redux';
+import { Link, useHref, useNavigate } from 'react-router-dom';
 
 const Search = () => {
     const [opened, setOpened] = useState<boolean>(false);
@@ -9,12 +13,33 @@ const Search = () => {
         setOpened(!opened);
     };
 
+    const {
+        register,
+        handleSubmit,
+    } = useForm()
+
+    const dispatch = useDispatch();
+
+    const navigate = useNavigate();
+
+    const goToCatalog = () => {navigate('/catalog')}
+
+    function getBySearch (data: any) {
+        console.log(data)
+        dispatch(setParam({
+            attribute: 'search', 
+            content: data.search
+        }));
+        goToCatalog()
+    }
+
+
     return (
         <div className={styles.block}>
             <button className={styles.searchTrigger} onClick={toggleForm}><img src={search} alt="search" /></button>
-            <form className={`${styles.form} ${opened && styles.formActive}`}>
-                <input className={styles.input} type="text" onBlur={toggleForm} />
-                <img className={styles.inputSearch} src={search} alt="search" />
+            <form className={`${styles.form} ${opened && styles.formActive}`} onSubmit={handleSubmit(getBySearch)}>
+                <input className={`search-input ${styles.input}`} type="text" {...register('search')} />
+                <button ><img className={styles.inputSearch} src={search} alt="search" /></button>
             </form>
         </div>
     );
